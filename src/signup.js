a4285c8f2757eaf052b402768adeb9893308e846
Dipanshu Gupta <131527196+dipanshugupta0011@users.noreply.github.com>
2023-10-17 10:06:39 +0530

    FirstCommit

--- src/signup.js
+++ src/signup.js
@@ -1,29 +1,47 @@
-function validateForm() {
-  var username = document.getElementById("username").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  var confirm_password = document.getElementById("confirm_password").value;
-  var error_message = document.getElementById("error-message");
+document
+  .getElementById("signupForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
 
-  if (
-    username === "" ||
-    email === "" ||
-    password === "" ||
-    confirm_password === ""
-  ) {
-    error_message.innerHTML = "Please fill in all required fields.";
-    return false; // Prevent form submission
-  }
+    var username = document.getElementById("username").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var confirm_password = document.getElementById("confirm_password").value;
+    var error_message = document.getElementById("error-message");
 
-  if (password !== confirm_password) {
-    error_message.innerHTML = "Passwords do not match!";
-    return false; // Prevent form submission
+    if (
+      username === "" ||
+      email === "" ||
+      password === "" ||
+      confirm_password === ""
+    ) {
+      error_message.innerText = "Please fill in all required fields.";
+      return false; // Prevent form submission
+    }
+
+    if (password !== confirm_password) {
+      error_message.innerText = "Passwords do not match!";
+      return false; // Prevent form submission
+    } else {
+      error_message.innerText = ""; // Clear any previous error messages
+      // If all fields are filled and passwords match, redirect to a new page (e.g., success.html)
+      window.location.href = "success.html";
+      // Commented out the redirection for demonstration purposes
+      document.getElementById("signupForm").reset(); // Reset the form after submission
+      return true; // Allow form submission
+    }
+  });
+
+function togglePassword() {
+  var passwordInput = document.getElementById("password");
+  var icon = document.querySelector(".password-toggle-icon");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    icon.classList.remove("fa-eye-slash");
+    icon.classList.add("fa-eye");
   } else {
-    error_message.innerHTML = ""; // Clear any previous error messages
-    // If all fields are filled and passwords match, redirect to a new page (e.g., success.html)
-    window.location.href = "success.html";
-    // Commented out the redirection for demonstration purposes
-    document.getElementById("signupForm").reset(); // Reset the form after submission
-    return false; // Prevent form submission
+    passwordInput.type = "password";
+    icon.classList.remove("fa-eye");
+    icon.classList.add("fa-eye-slash");
   }
 }
